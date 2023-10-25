fc3ed971a6bb0c15b5826d721b22daca73108219
Goran Topic <telegonicaxx@live.com>
2023-10-25 16:18:36 -0700

    working axios version. has to rewrite everything

--- test/parser.js
+++ test/parser.js
@@ -8,7 +8,7 @@ import fs from 'fs';
 
 
 // The test suite using Mocha's describe function
-describe('whisper api response', () => {
+describe('check that the ', () => {
     // Test case 1
     it('correcly transcribs the audio "The embellishment kits"', async () => {
         // make the post request
@@ -36,3 +36,76 @@ describe('whisper api response', () => {
 
 
 
+/*
+ * inconsistentHtmlContent: 
+Para conocer si te encuentras registrado en la base de datos del Registro Social, ingresa tu número de cédula:
+noDataHtmlContent: 
+Usted no consta en el Registro SocialNueva Consulta
+moreDataHtmlContent: 
+{
+  Sujeto: 'Sra. DELGADO MARIA ROCIO DE LOS ANGELES con cédula 0400558862, Usted consta en el Registro Social.',
+  Certificado: '7245545',
+  Provincia: 'PICHINCHA',
+  'Cantón': 'QUITO',
+  Parroquia: 'TUMBACO',
+  'Fecha entrevista': '23/09/2022',
+  'Datos de la familia': [
+    {
+      'Núcleo': '1',
+      Apellidos: 'DELGADO ',
+      Nombres: 'MARIA ROCIO DE LOS ANGELES',
+      'Cédula': '0400558862',
+      Edad: '64',
+      Parentesco: 'Jefe(a) de NÚCLEO  ',
+      Estado: 'Válido'
+    },
+    {
+      'Núcleo': '1',
+      Apellidos: 'MARTINEZ DELGADO',
+      Nombres: 'NANCY MIREYA',
+      'Cédula': '17XXXXXXXX',
+      Edad: '35',
+      Parentesco: 'Hijo(a)       ',
+      Estado: 'Válido'
+    },
+    {
+      'Núcleo': '1',
+      Apellidos: 'MARTINEZ DELGADO',
+      Nombres: 'CRISTHIAN ANTONIO',
+      'Cédula': '17XXXXXXXX',
+      Edad: '25',
+      Parentesco: 'Hijo(a)       ',
+      Estado: 'Válido'
+    }
+  ]
+}
+someDataHtmlContent: 
+{
+  Sujeto: 'Sra. QUITUIZACA TENEMAZA MARIA ETELVINA con cédula 0701593105, Usted consta en el Registro Social.',
+  Certificado: '5096558',
+  Provincia: 'AZUAY',
+  'Cantón': 'CAMILO PONCE ENRIQUEZ',
+  Parroquia: 'EL CARMEN DE PIJILI',
+  'Fecha entrevista': '03/09/2020',
+  'Datos de la familia': [
+    {
+      'Núcleo': '1',
+      Apellidos: 'QUITUIZACA TENEMAZA',
+      Nombres: 'MARIA ETELVINA',
+      'Cédula': '0701593105',
+      Edad: '62',
+      Parentesco: 'Jefe(a) de NÚCLEO  ',
+      Estado: 'Válido'
+    },
+    {
+      'Núcleo': '1',
+      Apellidos: 'LOZANO QUITUIZACA',
+      Nombres: 'ERNESTO JAVIER',
+      'Cédula': '07XXXXXXXX',
+      Edad: '26',
+      Parentesco: 'Hijo(a)       ',
+      Estado: 'Válido'
+    }
+  ]
+}
+*/
